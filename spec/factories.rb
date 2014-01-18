bd741e1b840dc5d1d14b286536645795adc728e1
Casey Diamantopoulos <the.dimo@gmail.com>
2014-01-17 22:27:02 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Casey Diamantopoulos"
+    email    "the.dimo@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
